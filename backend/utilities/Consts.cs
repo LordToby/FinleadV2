ecb8a2f44aa58395c0710f783ce2533df87cbdcd
Tobias <70690455+Tobias@users.noreply.github.com>
2023-08-19 16:46:07 +0200

    add const class

---
+++ backend/utilities/Consts.cs
@@ -0,0 +1,8 @@
+﻿namespace ElephantSQL_example.utilities
+{
+    public class Consts
+    {
+        public const string UserName = "andreyka26_";
+        public const string Password = "Mypass1*";
+    }
+}
